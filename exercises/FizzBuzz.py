7bc45b5d1fb43e7dc520500d7ceda2683f38e765
jols ade <jols ade>
2016-02-17 10:59:07 -0800

    The Fizz buzz division challenge

---
+++ exercises/FizzBuzz.py
@@ -0,0 +1,10 @@
+
+def fizz_buzz(num):
+  if ( num % 3 ==0 and num % 5 == 0):
+     return "FizzBuzz"
+  if( num % 3 ==0 ):
+    return "Fizz"
+  if( num %  5 == 0):
+    return "Buzz"
+    
+  return num
